678c750b136c706df79904651155b1184499dc50
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-01-13 08:36:26 -0300

    Mais um teste realizado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Retailer;
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Http\Request;
+
+
 class AuthController extends Controller
-{
-    public function postAuthenticate(String $provider) {
+{    
+    public function postAuthenticate(Request $request, String $provider) {
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
         $providers = ['user', 'realier'];
 
         if(!in_array($provider, $providers))
@@ -12,7 +24,29 @@ class AuthController extends Controller
             return response()->json(['errors' => ['main' => 'Wrong provider provided']], 422);
         }
 
+        $selectProvider = $this->getProvider($provider);
+
+        $model = $selectProvider->where('email', '=', $request->input('email'))->first();
+
+        if(!$model) {
+            return response()->json(['errors' => ['main' => 'Wrong credentials']], 401);
+        }
+
         return 'o provider escolhido foi'. $provider;
 
     }
+
+    public function getProvider(string $provider): AuthenticatableContract
+    {
+
+        if($provider == "user") {
+            return new User();
+        } else if ($provider == "retailer") {
+            return new Retailer();
+        } else {
+            throw new \Exception('Provider Not found');
+        }
+    }
+
+
 }
--- tests/Feature/app/Http/Controllers/AuthControllerTest.php
+++ tests/Feature/app/Http/Controllers/AuthControllerTest.php
@@ -2,10 +2,12 @@
 
 namespace Feature\app\Http\Controllers;
 
+use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\TestCase;
 
 class AuthControllerTest extends TestCase
 {
+    use DatabaseMigrations;
 
     public function createApplication() {
         return require './bootstrap/app.php';
@@ -13,10 +15,28 @@ class AuthControllerTest extends TestCase
 
     public function testUserShouldNotAuthenticateWithWrongProvider() {
 
-        $request = $this->post(route('authenticate', ['provider' => 'deixa-o-sub']));
+        $payload = [
+            'email' => 'hey@guilherme3s.dev',
+            'password' => 'secret123'
+        ];
+
+        $request = $this->post(route('authenticate', ['provider' => 'deixa-o-sub']), $payload);
 
         $request->assertResponseStatus(422);
         $request->seeJson(['errors' => ['main' => 'Wrong provider provided']]);
+    }
+
+    public function testUserShouldBeDeniedIfNoRegistered() {
+
+        $payload = [
+            'email' => 'hey@guilherme3s.dev',
+            'password' => 'secret123'
+        ];
+
+        $request = $this->post(route('authenticate', ['provider' => 'user']), $payload);
+
+        $request->assertResponseStatus(401);
+        $request->seeJson(['errors' => ['main' => 'Wrong credentials']]);
     }
 }
 
